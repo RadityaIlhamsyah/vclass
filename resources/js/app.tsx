c7c31c4ca5dbc4cc3955f1e789fc33dd695c7d1c
Raditya taufiq Ilhamsyah <159111710+RadityaIlhamsyah@users.noreply.github.com>
2025-01-16 22:07:33 +0700

    Update app.tsx

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -1,25 +1,13 @@
-import '../css/app.css';
-import './bootstrap';
-
-import { createInertiaApp } from '@inertiajs/react';
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import { createRoot } from 'react-dom/client';
-
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+// resources/js/app.tsx
+import { createInertiaApp } from '@inertiajs/react'
+import { createRoot } from 'react-dom/client'
 
 createInertiaApp({
-    title: (title) => `${title} - ${appName}`,
-    resolve: (name) =>
-        resolvePageComponent(
-            `./Pages/${name}.tsx`,
-            import.meta.glob('./Pages/**/*.tsx'),
-        ),
-    setup({ el, App, props }) {
-        const root = createRoot(el);
-
-        root.render(<App {...props} />);
-    },
-    progress: {
-        color: '#4B5563',
-    },
-});
+  resolve: name => {
+    const pages = import.meta.glob('./Pages/**/*.tsx')
+    return pages[`./Pages/${name}.tsx`]()
+  },
+  setup({ el, App, props }) {
+    createRoot(el).render(<App {...props} />)
+  },
+})
